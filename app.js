2d2025f649802650bac3119f0af93c1a9e1febac
yaser.beker <yaser.beker@indata.com.tr>
2025-01-20 12:03:47 +0300

    Implement battle log functionality and add surrender option

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
             monsterHealth: 100,
             currentRound: 0,
             winner: null,
+            logMessages: [],
         }
     },
     computed: {
@@ -50,21 +51,26 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.winner = null;
             this.currentRound = 0;
+            this.logMessages = []
         },
         attackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(5 , 12);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
+
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
         healPlayer(){
@@ -75,7 +81,19 @@ const app = Vue.createApp({
             }else{
                 this.playerHealth += healValue;              
             }
+            this.addLogMessage('player', 'heal', healValue);
             this.attackPlayer();
+        },
+        surrender(){
+            this.winner = 'monster';
+
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         },
     },
 });
